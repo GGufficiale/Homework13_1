69b1fcb8aa7f7a59b1cb2ea606e8df86f28f0015
GGufficiale <germangranovskii@mail.ru>
2024-04-14 15:13:29 +0300

    Исправлены Классы, исправлены тесты

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ class Category:
     name: str
     description: str
     products: list
+    all_categories = 0
+    all_unique_goods = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
-        self.categories = 0
-        self.unique_goods = 0
+        self.all_unique_goods = set
+
+        Category.all_categories += 1
 
 
 class Product:
@@ -23,15 +26,16 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.price = float(price)
         self.quantity = quantity
 
 
 def load_data_from_file():
     """Загрузка данных из файла .json"""
-    with open("products.json") as file:
+    with open("products.json", "r", encoding='utf-8') as file:
         data = json.load(file)
         product_list = []
         for product in data:
             product_list.append(Category(product['name'], product['description'], product["products"]))
     return product_list
+
--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from main import Product
 @pytest.fixture
 def category_object():
     """Тест получает на вход класс Category для удобства дальнейшей работы"""
-    return Category(str, str, list)
+    return Category('Шоколад', 'Вкусный', ["Риттер Спорт", "Щедрая душа"])
 
 
 def test_init_category(category_object):
@@ -14,23 +14,20 @@ def test_init_category(category_object):
     Тест проверяет корректность инициализации объектов класса Category.
     Также тест считает количество продуктов и категорий.
     """
-    assert category_object.name == str
-    assert category_object.description == str
-    assert category_object.products == list
-    assert category_object.categories >= 0
-    assert category_object.unique_goods >= 0
+    assert category_object.name == 'Шоколад'
+    assert category_object.description == 'Вкусный'
+    assert category_object.products == ["Риттер Спорт", "Щедрая душа"]
 
 
 @pytest.fixture
 def product_object():
     """Тест получает на вход класс Product для удобства дальнейшей работы"""
-    return Product(str, str, float, int)
+    return Product('Одежда', 'для занятия спортом', 599, 10)
 
 
 def test_init_product(product_object):
     """Тест проверяет корректность инициализации объектов класса Product"""
-    assert product_object.name == str
-    assert product_object.description == str
-    assert product_object.price == float
-    assert product_object.quantity == int
-
+    assert product_object.name == 'Одежда'
+    assert product_object.description == "для занятия спортом"
+    assert product_object.price == 599
+    assert product_object.quantity == 10
